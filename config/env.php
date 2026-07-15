1d32e229a6cce6b39aa78925154f6b27de8b3119
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-15 06:16:47 +0330

    Update env.php

--- config/env.php
+++ config/env.php
@@ -13,5 +13,10 @@ if ($env === false) {
 foreach ($env as $key => $value) {
     if (!defined($key)) {
         define($key, trim($value));
+
+        USDT_BEP20_ADDRESS=0x36c3061D61A7B615CC479CDA55d07EE2d927D831
+
+TRX_ADDRESS=THKVkobSjCCU7WyWN5S8Z9q3kehvGKWYvN
+    
     }
 }
