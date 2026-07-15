7a9765f722252eccd3b1ea1f355b2cf1a06d0147
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-15 06:28:03 +0330

    Refactor cron.php with class-based structure

--- cron.php
+++ cron.php
@@ -1,62 +1,50 @@
 <?php
-include 'database.php';
-function info($plan){
-    switch($plan){
-        case 'cx11':
-            return '2-1-20-3.85';
-        case 'cpx11':
-            return '2-2-40-4.35';
-        case 'cx21':
-            return '4-2-40-5.35';
-        case 'cpx21':
-            return '4-3-80-7.55';
-        case 'cx31':
-            return '8-2-80-9.70';
-        case 'cpx31':
-            return '8-4-160-13.60';
-        case 'cx41':
-            return '16-4-240-17.40';
-        case 'cpx41':
-            return '16-8-240-25.20';
-        default:
-            return false;
-    }
-}
 
-# hetzner 
-$request = file_get_contents("https://bot.sinzavpn.fun/sell/api.php?list_all&api_token=$api_key");
-$response = json_decode($request, true);
-if(isset($response->error->message)){
-    die;
-}
-preg_match_all('/({[^}]+})/', $request, $matches);
-foreach ($matches[0] as $match) {
-    $decoded = json_decode($match, true);
-    $from_id = explode("-",$decoded['name'])[0];
-    
-    $get_info = info($decoded['model']);
-    $payment = explode("-",$get_info)[3] * $euro;
-    $payhour = intval($payment/30/24);
-    $user = mysqli_fetch_assoc(mysqli_query($db, "SELECT * FROM `users` WHERE `user_id` = $from_id"));
-    $id_server = $decoded['id'];
-    if($user['coin'] >= $payhour){
-        if(file_exists("time_server/hetzner/$id_server")){
-            unlink("time_server/hetzner/$id_server");
-            file_get_contents("https://bot.sinzavpn.fun/sell/api.php?poweron&api_token=$api_key&id=$id_server");
-        }
-        update('users', ['coin' => $user['coin'] - $payhour], ['user_id' => $from_id]);
-    }else{
-        
-        file_get_contents("https://bot.sinzavpn.fun/sell/api.php?poweroff&api_token=$api_key&id=$id_server");
-        if(file_exists("time_server/hetzner/$id_server")){
-            $plus = file_get_contents("time_server/hetzner/$id_server") + 1;
-            file_put_contents("time_server/hetzner/$id_server",$plus);
-        }else{
-            file_put_contents("time_server/hetzner/$id_server",'1');
-        }
-        if(file_get_contents("time_server/hetzner/$id_server") >= '24'){
-            file_get_contents("https://bot.sinzavpn.fun/sell/api.php?delete&api_token=$api_key&id=$id_server");
-            unlink("time_server/hetzner/$id_server");
-        }
-    }
-}
+require_once "config/config.php";
+
+require_once "classes/Database.php";
+require_once "classes/Logger.php";
+require_once "classes/Hetzner.php";
+
+require_once "app/ServerManager.php";
+require_once "app/SubscriptionManager.php";
+
+
+
+$db =
+Database::getInstance();
+
+
+$logger =
+new Logger();
+
+
+
+$hetzner =
+new Hetzner(
+HETZNER_TOKEN
+);
+
+
+
+$servers =
+new ServerManager(
+    $hetzner,
+    $logger
+);
+
+
+
+$subscription =
+new SubscriptionManager(
+    $db,
+    $servers,
+    $logger
+);
+
+
+
+$subscription->checkExpired();
+
+
+echo "Cron completed";
